da49d0d008b5382c134ebdf1252b0a5b0768b57d
StephenHarney <g00367573@atu.ie>
2023-11-08 09:00:40 +0000

    Exam

---
+++ src/main/java/ie/atu/hello/CicdExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdExamApplication.class, args);
+	}
+
+}
